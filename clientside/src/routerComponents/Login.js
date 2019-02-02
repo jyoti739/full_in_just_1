9f3d9e08d8be05ff56f75edf13b9985d6098f4fb
jyoti <jyotiranjan739@gmail.com>
2019-02-02 18:23:03 +0530

    this commit is from master

---
+++ clientside/src/routerComponents/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Menu}from 'semantic-ui-react'
+// import {}
+const Login = props =>{
+    return(
+        <div>
+						<div className = "segment" style = {{height : '200px', width : '200px', background : "blue",
+					color : "blue"}}>
+						
+						</div>
+        </div>
+    )
+}
+
+export default Login
