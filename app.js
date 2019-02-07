bbfd89116e82de1bc1cee503b5037f48bde28076
jyoti <jyotiranjan739@gmail.com>
2019-02-08 03:11:46 +0530

    express().static()

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const express = require('express')
+// const express = require('express')
 const app  = require('./express_1')
 const path = require('path')
 const server = http.createServer(app)
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 // Serve static assets if it is in production
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('clientside/build'))
+    app.use(app.static('clientside/build'))
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'clientside', 'build', 'index.html'))
     })
